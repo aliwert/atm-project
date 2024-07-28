4e09efbba8f5125f9ee2c956486c7a9fa046e655
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-29 01:01:45 +0300

    users

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        double dilanBalance = 50000;
+        double alimertBalance = 40000;
+
+        String dilanIBAN= "TR00 0000 0000 0000 0000 00";
+        String alimertIBAN = "TR11 1111 1111 1111 1111 11";
+
+        String dilanUser = "dilan";
+        String alimertUser= "aliwert";
+
+        String dilanPassword = "1234";
+        String alimertPassword="5678";
+
+
+
 
 
     }
