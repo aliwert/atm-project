746f23e7d6172f73fa954802f1822394957c0fc7
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-29 01:06:54 +0300

    scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
 
         double dilanBalance = 50000;
         double alimertBalance = 40000;
@@ -14,6 +20,16 @@ public class Main {
 
         String dilanPassword = "1234";
         String alimertPassword="5678";
+
+        System.out.println("Welcome to ATM");
+        System.out.println("Please enter your information");
+
+        System.out.println("Username: ");
+        String username= scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String password=scanner.nextLine();
+
 
 
 
